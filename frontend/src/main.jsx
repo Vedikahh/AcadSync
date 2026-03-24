d506a41136f8e59bdc41c264a28b89af2c3607a1
Omm-codes <omsanjay975@gmail.com>
2026-03-24 18:49:16 +0530

    vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
+      <Analytics />
     </GoogleOAuthProvider>
   </StrictMode>,
 )
